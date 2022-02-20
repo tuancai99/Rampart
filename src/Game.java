d266a317e0d21046a58d86e9a4216ed42acb1638
Tuan Cai <tuancai@lawn-143-215-90-50.lawn.gatech.edu>
2022-02-20 17:01:28 -0500

    updated code for Game sscreen

--- src/Game.java
+++ src/Game.java
@@ -18,9 +18,73 @@ public class Game extends Application {
 
 	@Override
 	public void start(Stage stage) throws FileNotFoundException {
-		Pane myPane = new Pane();
-        Scene scene = new Scene(myPane);
+
+
+		Image image = new Image(new FileInputStream("myMap.jpeg"));
+
+		ImageView imageView = new ImageView(image);
+
+		 imageView.setX(0);
+		 imageView.setY(0);
+
+		 imageView.setFitHeight(1200);
+		 imageView.setFitWidth(1450);
+
+		 imageView.setPreserveRatio(true);
+
+		//Tuan's // 
+		int startingMoney = 0;
+		int startingHealth = 0;
+		String charName = Player.getName();
+		int levelDif = Player.getLevel();
+
+		if (levelDif = 1) {
+			startingMoney =  1000;
+			startingHealth = 300;
+		} else if (levelDif = 2) {
+			startingMoney =  1500;
+			startingHealth = 400;
+		} else if ( levelDif = 3) {
+			startingMoney =  2000;
+			starting = 500;
+		}
+		Text text = new Text(); 
+
+		String moneyStr = "MONEY: " + String.valueOf(startingMoney);
+		String healthStr = "HEALTH: " + String.valueOf(startingHealth) + "hp";
+        //Tuan's // 
+
+
+      	 text.setFont(Font.font("verdana", FontWeight.BOLD, FontPosture.REGULAR, 20)); 
+      
+      	 text.setX(50); 
+      	 text.setY(100);    
+
+      	 //Tuan's //       
+      // change from MONEY: 200 to moneyStr vvvv
+      	 text.setText(moneyStr); 
+      	 //Tuan's // 
+
+
+      	 
+      	Text text2 = new Text(); 
+        
+      	 text2.setFont(Font.font("verdana", FontWeight.BOLD, FontPosture.REGULAR, 20)); 
+      
+      	 text2.setX(1000); 
+      	 text2.setY(100);    
+
+      	 //Tuan's //       
+      // change from HEALTH: 500 to healthStr vvvv
+      	 text2.setText(healthStr); 
+      	 //Tuan's // 
+
+		Group root = new Group(imageView, text, text2);
+
+        Scene scene = new Scene(root);
+
         stage.setScene(scene);
+        stage.setResizable(true);
         stage.show();
 
     }
