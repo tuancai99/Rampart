4649b65b706e47a065803719ed13a6c052be2fcb
ekim390 <ekim390@gatech.edu>
2022-03-08 16:47:23 -0500

    edited prices of towers

--- src/Tower3.java
+++ src/Tower3.java
@@ -1,43 +1,43 @@
 public class Tower3 implements Tower {
-	private int price;
-	private int xVal;
-	private int yVal;
-	private double dps;
+    private int price;
+    private int xVal;
+    private int yVal;
+    private double dps;
 
-	public Tower3(int dif) {
-		price = 150 * dif;
-		dps = 6 - (0.4(dif - 1));
-	}
+    public Tower3(int dif) {
+        price = 30 * dif;
+        dps = 6 - (0.4(dif - 1));
+    }
 
-	public void setPrice(int p) {
-		price = p;
-	}
+    public void setPrice(int p) {
+        price = p;
+    }
 
-	public int getPrice() {
-		return price;
-	}
+    public int getPrice() {
+        return price;
+    }
 
-	public void setxVal(int x) {
-		xVal = x;
-	}
+    public void setxVal(int x) {
+        xVal = x;
+    }
 
-	public int getxVal() {
-		return xVal;
-	}
+    public int getxVal() {
+        return xVal;
+    }
 
-	public void setyVal(int y) {
-		yVal = y;
-	}
+    public void setyVal(int y) {
+        yVal = y;
+    }
 
-	public int getyVal(){
-		return yVal;
-	}
+    public int getyVal(){
+        return yVal;
+    }
 
-	public void setdps(int d) {
-		dps = d;
-	}
+    public void setdps(int d) {
+        dps = d;
+    }
 
-	public double getdps() {
-		return dps;
-	}
+    public double getdps() {
+        return dps;
+    }
 }
