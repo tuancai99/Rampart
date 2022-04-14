f0801d7b65db746f0ea3393254b6c9ee5fd70c80
Tuan Cai <tcai44@gatech.edum>
2022-04-14 00:38:12 -0400

    testDistCal

---
+++ src/test/TowerTest.java
@@ -0,0 +1,33 @@
+package test;
+
+import javafx.application.Application;
+import main.*;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class TowerTest {
+    @BeforeClass
+    public static void setUpClass() throws InterruptedException {
+        // Initialise Java FX
+        Thread t = new Thread("JavaFX GameStart Thread") {
+            public void run() {
+                Application.launch(GameStart.class);
+            }
+        };
+        t.setDaemon(true);
+        t.start();
+        Thread.sleep(500);
+    }
+
+    @Test
+    public void distCalculator() {
+        Enemy myEnemy = new Enemy1(20,20);
+        Tower myTow = new Tower1();
+        myTow.setXVal(10);
+        myTow.setYVal(10);
+        assertEquals(myTow.distCalculator(myEnemy),
+                10*Math.sqrt(2), (Math.abs(myTow.distCalculator(myEnemy) - 10 * Math.sqrt(2))));
+    }
+}
